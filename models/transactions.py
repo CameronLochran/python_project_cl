9a4aa60f660f76496483fd0e97f250c575e7ab24
y <y>
2023-08-28 14:47:20 +0100

    WeeCameron

---
+++ models/transactions.py
@@ -0,0 +1,6 @@
+class Transactions:
+    def __init__(self, merchant, tag, amount, id = None):
+        self.merchant = merchant
+        self.tag = tag
+        self.amount = amount
+        self. id = id
